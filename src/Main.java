e64e52d4081bfdac2ed6b4a2bebe73edbea98adb
antlanc <antonio.lanciotti@live.it>
2019-03-11 17:29:44 +0100

    Creata interfaccia sul main

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,16 @@
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Scanner input = new Scanner(System.in);
+		System.out.print("f(x) = ");
+		Evaluator e = new Evaluator(input.nextLine());
+		System.out.print("x = ");
+		double x=input.nextDouble();
+		System.out.println(e.f(x));
 	}
 
 }
