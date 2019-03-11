74ecdc16c732e5ddf4790404db41d8fa76b3fc54
antlanc <antonio.lanciotti@live.it>
2019-03-11 17:40:41 +0100

    Interfaccia per integrali

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,18 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
 		Scanner input = new Scanner(System.in);
 		System.out.print("f(x) = ");
 		Evaluator e = new Evaluator(input.nextLine());
 		System.out.print("x = ");
 		double x=input.nextDouble();
 		System.out.println(e.f(x));
+		System.out.print("a = ");
+		double a=input.nextDouble();
+		System.out.print("b = ");
+		double b=input.nextDouble();
+		System.out.println(e.integralRect(a,b,1000));
 	}
 
 }
