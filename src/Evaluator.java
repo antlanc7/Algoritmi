f6e891d6d7c1f5e5a5185fcbbf900dc8e18ce44f
antlanc <antonio.lanciotti@live.it>
2019-03-11 17:23:18 +0100

    creato costruttore Evaluator

--- src/Evaluator.java
+++ src/Evaluator.java
@@ -2,6 +2,9 @@ import net.objecthunter.exp4j.*;
 
 
 public class Evaluator {
-	
+	private Expression expression;
+	Evaluator(String f){
+		expression=new ExpressionBuilder(f).variable("x").build();
+	}
 	
 }
