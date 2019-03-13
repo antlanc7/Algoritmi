3236ef68f2b21bbf04b5cdbb2bad5fd4793961d6
LorenzoDag <s1085177@studenti.univpm.it>
2019-03-13 21:53:31 +0100

    aggiunto metodo di simpson

--- src/Evaluator.java
+++ src/Evaluator.java
@@ -1,5 +1,5 @@
 import net.objecthunter.exp4j.*;
-
+import java.lang.Math.*;
 
 public class Evaluator {
 	private Expression expression;
@@ -47,7 +47,47 @@ public class Evaluator {
 	}
 	
 	
+	public double integrateSimpson(double a, double b, int n, double prec) {
+		
+		if (n%2 != 0) n++; 
+		
+		double h = (b-a)/n;
+		double sp = 0;
+		double sd = 0;
+		double x = a;
+		for (int i = 1; i <= (n-2)/2; i++) {
+			x += h;
+			sd += f(x);
+			x += h;
+			sp += f(x);
+		}
+		x += h;
+		sd += f(x);
+		double FAB = f(a) + f(b);
+		double area = (FAB + 4*sd + 2*sp)*h/3;
+		double area1, diff;
+		do {
+			sp += sd;
+			sd = 0;
+			x = a + h/2;
+			for(int i = 1; i <= n; i++) {
+				sd += f(x);
+				x += h;
+			}
+			area1 = (FAB +4*sd + 2*sp)*h/6;
+			diff = Math.abs(area1-area);
+			h = h/2;
+			n = n*2;
+		} while(diff > prec);
+		
+		return area;
+	}
 	
+	public double integrateSimpson(double a, double b, int n) {
+		
+		return integrateSimpson(a, b, n, 0.0001);
+		
+	}
 	
 	
 	public double derivate(double x0, int prec) {
